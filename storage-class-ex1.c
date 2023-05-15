95a37de0f38be72e1fa632f9e802399dd0be3932
Jake Peterson <a3sir86@gmail.com>
2023-05-15 10:16:28 -0500

    Update storage-class-ex1.c

--- storage-class-ex1.c
+++ storage-class-ex1.c
@@ -1,1 +1,25 @@
+//global double var accessible only inside this file
+static double y = 1.1;
 
+//global float var accessible within in entire program
+float z = 1.2;
+
+//function only accessible within this file
+static int add(int num1, int num2) {
+  int total = num1 + num2;
+  return total;
+}
+
+int main() {
+  
+  // int variable with block scope and temp storage
+  int x = 0;
+  
+  //float local var with permanent storage
+  static float a = 2.0;
+  
+  //register int variable
+  register int b = 0;
+   
+  return 0;
+}
