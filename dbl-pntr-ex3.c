66a5451f8947279a97b9b56166a7d9cc35ee633d
Jake Peterson <a3sir86@gmail.com>
2023-05-15 14:34:11 -0500

    Update dbl-pntr-ex3.c

--- dbl-pntr-ex3.c
+++ dbl-pntr-ex3.c
@@ -3,14 +3,14 @@
 #include <malloc.h>
 #include <string.h>
 
-void foo(char *ptr) {
-  ptr = malloc(255);
-  strcpy(ptr, "Hello World");
+void foo(char **ptr) {
+  *ptr = malloc(255);
+  strcpy(*ptr, "Hello World");
 }
 
 int main() {
   char *ptr = NULL;
-  foo(ptr);
+  foo(&ptr);
   
   printf("%s\n", ptr);
   
