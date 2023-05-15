21d9ed98ad0083c1f399ac9fec37cbd90472a071
Jake Peterson <a3sir86@gmail.com>
2023-05-15 14:31:19 -0500

    Create dbl-pntr-ex3.c

---
+++ dbl-pntr-ex3.c
@@ -0,0 +1,19 @@
+//this example shows another use case for using a double pointer in which you want to dynamically allocate memory for a pointer passed into a function
+
+#include <malloc.h>
+#include <string.h>
+
+void foo(char *ptr) {
+  ptr = malloc(255);
+  strcpy(ptr, "Hello World");
+}
+
+int main() {
+  char *ptr = NULL;
+  foo(ptr);
+  
+  printf("%s\n", ptr);
+  
+  free(ptr);
+  return 0;
+}
