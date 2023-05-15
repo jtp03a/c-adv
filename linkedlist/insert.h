4344f4e8262358c7a90bb9f28437e157b9d50e0e
Jake Peterson <a3sir86@gmail.com>
2023-05-15 08:38:11 -0500

    Update insert.h

--- linkedlist/insert.h
+++ linkedlist/insert.h
@@ -1,1 +1,5 @@
-
+struct Node;
+struct Node* create_node(int data);
+void b_insert(struct Node** head, int data);
+void n_insert(struct Node** head, int data, int pos);
+void e_insert(struct Node** head, int data);
