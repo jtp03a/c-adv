686b1aa9addc6c57539b02bf6f698cfa48273c77
Jake Peterson <a3sir86@gmail.com>
2023-05-15 15:59:23 -0500

    Create func-pntr-ex4.c

---
+++ func-pntr-ex4.c
@@ -0,0 +1,4 @@
+/**
+Write a program that will perform some arithmetic operations (user entry) on an array using function pointers
+The program will perform the user selected operation on two hard coded arrays
+**/
