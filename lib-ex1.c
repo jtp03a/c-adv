495c18634756b1b2ac2a6f07ea3000845b7e9ebc
Jake Peterson <a3sir86@gmail.com>
2023-05-16 08:38:41 -0500

    Update lib-ex1.c

--- lib-ex1.c
+++ lib-ex1.c
@@ -2,8 +2,14 @@
 
 /**
 Commands for linux
+   Create the libary
    gcc -c myLib.c -o lib_mylib.o
+   ar rcs lib_mylib.a lib_mylib.o
    
+   change to the main program
+   gcc -I ../pathto/ -c main.c -o main.o
+   link to libary
+   gcc -o main main.o ../pathtolib/lib_mylib.a
 **/
 
 #include <stdio.h>
