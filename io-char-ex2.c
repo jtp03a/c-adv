b8712be4ba79ac5fe3b094100dd4b0db26c85a57
Jake Peterson <a3sir86@gmail.com>
2023-05-16 12:51:02 -0500

    Create io-char-ex2.c

---
+++ io-char-ex2.c
@@ -0,0 +1,4 @@
+//write a program to convert uppercase to lowercase and vice versa
+//can take cla for name of file or ask user
+//create a temp file to store result and then rename temp file back to original
+//user issupper, tolower, toupper, or ascii conversion
