a5c90d35c02d8763d821454a98a02897df457bf0
Jake Peterson <a3sir86@gmail.com>
2023-05-16 10:34:21 -0500

    Create linked-list-sorter.c

---
+++ practice/linked-list-sorter.c
@@ -0,0 +1,2 @@
+//write a program that reads a file of integers, puts them in a linked list, and sorts the linked list using selection sort, bubble sort, insertion sort, merge sort, and quick sort.
+
