d44b14f93a8ed3de9410580a6ab817af63c64650
Jake Peterson <a3sir86@gmail.com>
2023-05-16 13:37:16 -0500

    Update io-char-ex1.c

--- io-char-ex1.c
+++ io-char-ex1.c
@@ -2,3 +2,36 @@
 //the program can take zero cmd line args or one command line arg
 //if there is one arg it is the name of the file and that will be the input
 //if there is no arg then stdin is to be used for input
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+  int wordCount = 0;
+  int charCount = 0;
+  
+  int ch = 0;
+  
+  FILE *fp = NULL;
+  
+  fp = fopen("data", "r");
+  
+  //get first char and word
+  if ((ch = fgetc(fp)) !=EOF){
+      wordCount++;
+      charCount++;
+  }
+  
+  //get the rest
+  while ((ch = fgetc(fp)) != EOF) {
+    if (isspace(ch)) {
+        wordCount++;
+    }
+    if (ch != '\n') {
+     charCount++;    
+    }
+  }
+  
+  printf("Characters in the file: %d\n", charCount);
+  printf("Words in the file %d\n", wordCount);
+}
