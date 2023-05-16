891d9f0e2378626a78be5c752afa79d7316a530e
Jake Peterson <a3sir86@gmail.com>
2023-05-16 13:47:24 -0500

    Update io-char-ex2.c

--- io-char-ex2.c
+++ io-char-ex2.c
@@ -2,3 +2,28 @@
 //can take cla for name of file or ask user
 //create a temp file to store result and then rename temp file back to original
 //user issupper, tolower, toupper, or ascii conversion
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp = NULL;
+    FILE *tmp = NULL;
+    fp = fopen("data", "r");
+    tmp = fopen("tmp", "w");
+    
+    int ch = 0;
+    
+    while((ch = fgetc(fp)) != EOF) {
+        if (isupper(ch)) {
+            fputc(tolower(ch), tmp);
+        } else {    
+            fputc(toupper(ch), tmp);
+        }
+    }
+    
+    fclose(fp);
+    fclose(tmp);
+    
+    rename("tmp", "data");
+}
