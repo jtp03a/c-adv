9cbb5a0107ab793357359c15e4f88ccd020cf051
Jake Peterson <a3sir86@gmail.com>
2023-05-16 12:49:26 -0500

    Create io-char-ex1.c

---
+++ io-char-ex1.c
@@ -0,0 +1,4 @@
+//write a program to count the number of words and characters in a file OR from standard input
+//the program can take zero cmd line args or one command line arg
+//if there is one arg it is the name of the file and that will be the input
+//if there is no arg then stdin is to be used for input
